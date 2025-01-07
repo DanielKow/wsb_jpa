3db5a46dc0c9838128a343d216120bd81facbaa1
Daniel Kowalski <daniel@changelog.pl>
2025-01-07 17:53:07 +0100

    Changes by awawr

--- wsb-jpa1/src/main/java/com/jpacourse/service/PatientService.java
+++ wsb-jpa1/src/main/java/com/jpacourse/service/PatientService.java
@@ -1,8 +1,13 @@
 package com.jpacourse.service;
 
 import com.jpacourse.dto.PatientTO;
+import java.util.List;
+
 
 public interface PatientService {
     public PatientTO findById(final Long id);
     public void remove(final Long id);
+
+    List<PatientTO> getByLastName (String lastName);
+
 }
